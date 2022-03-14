e75504d36a33d132612f73d8ac4750ae48e784a9
Michael Chang <mchang9607@gmail.com>
2022-03-14 16:02:02 -0500

    Added MethodData class for data accessability. Further implemented BytecodeAnalyzer and (temporarily) discarded usage of javassist.

--- src/main/java/edu/illinois/randooop/APIAnalyzer.java
+++ src/main/java/edu/illinois/randooop/APIAnalyzer.java
@@ -2,9 +2,12 @@ package edu.illinois.randooop;
 
 import java.io.File;
 import java.util.List;
+import javassist.CtClass;
 
 public interface APIAnalyzer {
-	public File getFile(String path);
+	public CtClass getClassObject();
 	public List<String> getMethodSignatures();
-	public List<List<String>> parseMethodSignatures();
+	public List<String> getMethodNames();
+	public List<List<String>> getMethodArguments();
+	public List<String> getMethodReturnTypes();
 }
--- src/main/java/edu/illinois/randooop/App.java
+++ src/main/java/edu/illinois/randooop/App.java
@@ -1,13 +1,64 @@
 package edu.illinois.randooop;
 
+import java.io.File;
+import java.lang.reflect.AnnotatedType;
+import java.lang.reflect.Method;
+import java.net.URL;
+import java.net.URLClassLoader;
+import java.util.ArrayList;
+import java.util.List;
+
+import javassist.ClassPool;
+import javassist.CtClass;
+import javassist.CtMethod;
+
+
+
 /**
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+    public static void main( String[] args ) {
+    	
+        try {
+        	
+        	File file = new File("./");
+        	URL url = file.toURI().toURL();
+        	URL[] urls = new URL[] {url};
+        	ClassLoader cl = new URLClassLoader(urls);
+        	Class<?> cls = cl.loadClass("edu.illinois.randooop.BytecodeAnalyzer");
+        	Method[] methods = cls.getMethods();
+        	for (Method method : methods) {
+        		//System.out.println(method.getReturnType().toGenericString());
+        		//System.out.println(method.getReturnType());
+        		System.out.println(method.getName());
+        		System.out.println(method.toString());
+        		System.out.println(method.toGenericString());
+        		//*
+        		AnnotatedType[] p = method.getAnnotatedParameterTypes();
+        		for (AnnotatedType a : p) {
+        			System.out.println(a.toString());
+        		}
+        		//	*/
+        	}
+         	ClassPool pool = ClassPool.getDefault();
+        	pool.appendClassPath("./");
+        	CtClass ex = pool.get("edu.illinois.randooop.BytecodeAnalyzer");
+        	ex.defrost();
+        	
+        	CtMethod[] ctmethods = ex.getDeclaredMethods();
+        } catch (Exception e) {
+        	System.out.println("File Error");
+        	e.printStackTrace();
+        }
     }
+    
+    public int methodTest1(int x) {
+    	return 0;
+    }
+    private double methodTest1(boolean x) {
+    	return 0.0;
+    }
+    
 }
