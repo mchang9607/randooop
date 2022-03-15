909ddef9f8d80d7f8987bac08d78401b20d1f1c0
Michael Chang <mchang9607@gmail.com>
2022-03-15 01:34:17 -0500

    Implementation of validateAPIInput and alternative for generateAPIList. Further implemented a toString method.

--- src/main/java/edu/illinois/randooop/App.java
+++ src/main/java/edu/illinois/randooop/App.java
@@ -6,6 +6,7 @@ import java.lang.reflect.Method;
 import java.net.URL;
 import java.net.URLClassLoader;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
 import javassist.ClassPool;
@@ -22,8 +23,15 @@ public class App {
     public static void main( String[] args ) {
     	
         try {
-        	
         	File file = new File("./");
+        	BytecodeAnalyzer bca = new BytecodeAnalyzer();
+        	HashMap<String, MethodData> metadata = bca.generateAPIList(bca.getClassObject(file, "edu.illinois.randooop.BytecodeAnalyzer"));
+        	for (String key : metadata.keySet()) {
+        		System.out.println("Key: " + key);
+        		System.out.println("Value:\n" + metadata.get(key));
+        		System.out.println();
+        	}
+        	/*
         	URL url = file.toURI().toURL();
         	URL[] urls = new URL[] {url};
         	ClassLoader cl = new URLClassLoader(urls);
@@ -35,30 +43,17 @@ public class App {
         		System.out.println(method.getName());
         		System.out.println(method.toString());
         		System.out.println(method.toGenericString());
-        		//*
         		AnnotatedType[] p = method.getAnnotatedParameterTypes();
         		for (AnnotatedType a : p) {
         			System.out.println(a.toString());
         		}
-        		//	*/
-        	}
-         	ClassPool pool = ClassPool.getDefault();
-        	pool.appendClassPath("./");
-        	CtClass ex = pool.get("edu.illinois.randooop.BytecodeAnalyzer");
-        	ex.defrost();
+        	//	*/
         	
-        	CtMethod[] ctmethods = ex.getDeclaredMethods();
+        	
+         	
         } catch (Exception e) {
         	System.out.println("File Error");
         	e.printStackTrace();
         }
     }
-    
-    public int methodTest1(int x) {
-    	return 0;
-    }
-    private double methodTest1(boolean x) {
-    	return 0.0;
-    }
-    
 }
